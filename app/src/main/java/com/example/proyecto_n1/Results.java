cc719e28a45348cb4b5edd399730102072c16fbb
Eivar Morales <eivaremir@gmail.com>
2023-05-08 18:32:42 -0500

    created computeResults function

--- app/src/main/java/com/example/proyecto_n1/Results.java
+++ app/src/main/java/com/example/proyecto_n1/Results.java
@@ -75,80 +75,13 @@ public class Results extends AppCompatActivity {
         DynamoClient.list("PK", "VOTACION", new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-                System.out.println("RESPONSE" + response);
-                JSONObject p = null;
-
-
-                    try {
-                        pts = new int[ parts.length()][6];
-
-                        // por cada participante
-                        for(int i =0; i< parts.length();i++) {
-                            Integer pk = Integer.parseInt((String) parts.getJSONObject(i).getString("PK").split("#")[1]);
-
-                            // pts i es un participante
-                            // pts 0 es el id, 1 es el puntaje sumado y 2 es la posicion del finalista
-                            pts[i][0]=pk; // id del participante
-                            System.out.println(pk);
-
-                            // por cada votacion
-                            for(int v  =0;v<response.length();v++){
-                                JSONObject votacion = response.getJSONObject(v);
-                                Integer id_participant = Integer.parseInt(votacion.getString("SK").split("#")[3]);
-
-                                proyeccion = (Integer.parseInt(votacion.getString("proyeccion")));
-                                lenguaje = (Integer.parseInt(votacion.getString("lenguaje")));
-                                contenido = (Integer.parseInt(votacion.getString("contenido")));
-
-                                // si la votacion v corresponde al participante i
-                                if (id_participant == pk){
-                                    System.out.println("Participant "+id_participant);
-                                    pts[i][1]+= (proyeccion);
-                                    pts[i][2]+= (lenguaje);
-                                    pts[i][3]+= (contenido);
-                                    pts[i][4]+= (proyeccion+lenguaje+contenido);
-                                }
-                            }
-                        }
-                        System.out.println("Column 1\tColumn 2");
-
-                        Arrays.sort(pts, new Comparator<int[]>() {
-                            @Override
-                            public int compare(int[] a, int[] b) {
-                                return Integer.compare(b[1], a[1]);
-                            }
-                        });
-
-
-
-                        Integer max_index = 0;
-                        Integer posicion = 1;
-                        pts[0][5]=posicion;
-                        for(int i=1; i<pts.length;i++){
-                            int current = pts[i][1];
-                            int previous = pts[i-1][1];
-
-                            if (current==previous){
-                                max_index +=1;
-
-                            }
-                            else{
-                                max_index +=1;
-                                posicion+=1;
-                            }
-                            pts[i][5] = posicion;
-                        }
-                        for (int i = 0; i < pts.length; i++) { //part_puntaje.length
-                            for (int j = 0; j < pts[1].length; j++) {
-                                System.out.print(pts[i][j] + "\t\t");
-                            }
-                            System.out.println();
-                        }
-                    } catch (JSONException e) {
-                        throw new RuntimeException(e);
+                pts = computeResults(response);
+                for (int i = 0; i < pts.length; i++) { //part_puntaje.length
+                    for (int j = 0; j < pts[1].length; j++) {
+                        System.out.print(pts[i][j] + "\t\t");
                     }
-
-
+                    System.out.println();
+                }
 
             }
         });
@@ -160,7 +93,81 @@ public class Results extends AppCompatActivity {
             }
         });
     }
+    public int[][] computeResults(JSONArray response){
+        System.out.println("RESPONSE" + response);
+        JSONObject p = null;
+        int[][] pts ;
 
+        try {
+            pts = new int[ parts.length()][6];
+
+            // por cada participante
+            for(int i =0; i< parts.length();i++) {
+                Integer pk = Integer.parseInt((String) parts.getJSONObject(i).getString("PK").split("#")[1]);
+
+                // pts i es un participante
+                // pts 0 es el id, 1 es el puntaje sumado y 2 es la posicion del finalista
+                pts[i][0]=pk; // id del participante
+                System.out.println(pk);
+
+                // por cada votacion
+                for(int v  =0;v<response.length();v++){
+                    JSONObject votacion = response.getJSONObject(v);
+                    Integer id_participant = Integer.parseInt(votacion.getString("SK").split("#")[3]);
+
+                    proyeccion = (Integer.parseInt(votacion.getString("proyeccion")));
+                    lenguaje = (Integer.parseInt(votacion.getString("lenguaje")));
+                    contenido = (Integer.parseInt(votacion.getString("contenido")));
+
+                    // si la votacion v corresponde al participante i
+                    if (id_participant == pk){
+                        System.out.println("Participant "+id_participant);
+                        pts[i][1]+= (proyeccion);
+                        pts[i][2]+= (lenguaje);
+                        pts[i][3]+= (contenido);
+                        pts[i][4]+= (proyeccion+lenguaje+contenido);
+                    }
+                }
+            }
+            System.out.println("Column 1\tColumn 2");
+
+            Arrays.sort(pts, new Comparator<int[]>() {
+                @Override
+                public int compare(int[] a, int[] b) {
+                    return Integer.compare(b[1], a[1]);
+                }
+            });
+
+
+
+            Integer max_index = 0;
+            Integer posicion = 1;
+            pts[0][5]=posicion;
+            for(int i=1; i<pts.length;i++){
+                int current = pts[i][1];
+                int previous = pts[i-1][1];
+
+                if (current==previous){
+                    max_index +=1;
+
+                }
+                else{
+                    max_index +=1;
+                    posicion+=1;
+                }
+                pts[i][5] = posicion;
+            }
+            for (int i = 0; i < pts.length; i++) { //part_puntaje.length
+                for (int j = 0; j < pts[1].length; j++) {
+                    System.out.print(pts[i][j] + "\t\t");
+                }
+                System.out.println();
+            }
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        return pts;
+    }
     /*public int CantidadParticipantes() {
         int[] amount = new int[1];
         DynamoClient.list("PK", "PARTICIPANT", new JsonHttpResponseHandler() {
