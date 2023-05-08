dace7b8d980fe89870a5f08f42defc3e8c9f8270
Eivar Morales <eivaremir@gmail.com>
2023-05-08 17:55:32 -0500

    finished basic algorithm for computing results

--- app/src/main/java/com/example/proyecto_n1/Results.java
+++ app/src/main/java/com/example/proyecto_n1/Results.java
@@ -26,21 +26,48 @@ public class Results extends AppCompatActivity {
     int[][] pts ;
     String[] names;
     JSONArray parts ;
+    public class ParticipantsResponseHandler extends JsonHttpResponseHandler {
+        public ParticipantsResponseHandler() {
+            super();
+            System.out.println("init Http handler ");
+            finish = false;
+        }
+        @Override
+        public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+            System.out.println("RESPONSE(RESULTS)" + response);
+            cantidad_parti = response.length();
+            parts = response;
+            System.out.println("CANTIDAD DE PARTICIPANTES(RESULTS)" + cantidad_parti);
+            setFinish();
+        }
+        Boolean finish ;
+        public synchronized void setFinish(){
+            finish = true;
+        }
 
+        public synchronized boolean isFinished(){
+            return finish;
+        }
+    }
+    JsonHttpResponseHandler participantsRequestHandler =new JsonHttpResponseHandler() {
+        @Override
+        public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+            System.out.println("RESPONSE(RESULTS)" + response);
+            cantidad_parti = response.length();
+            parts = response;
+            System.out.println("CANTIDAD DE PARTICIPANTES(RESULTS)" + cantidad_parti);
+        //    setFinish();
+        }
+
+
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
 
-        DynamoClient.list("PK", "PARTICIPANT", new JsonHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-                System.out.println("RESPONSE(RESULTS)" + response);
-                cantidad_parti = response.length();
-                parts = response;
-                System.out.println("CANTIDAD DE PARTICIPANTES(RESULTS)" + cantidad_parti);
-            }
-        });
+        DynamoClient.list("PK", "PARTICIPANT",participantsRequestHandler);
+
 
         //cantidad_parti = CantidadParticipantes();
         //System.out.println("CANTIDAD DE PARTICIPANTES (RESULTS)" + cantidad_parti);
@@ -53,8 +80,9 @@ public class Results extends AppCompatActivity {
 
 
                     try {
-                        pts = new int[ parts.length()][3];
+                        pts = new int[ parts.length()][6];
 
+                        // por cada participante
                         for(int i =0; i< parts.length();i++) {
                             Integer pk = Integer.parseInt((String) parts.getJSONObject(i).getString("PK").split("#")[1]);
 
@@ -62,81 +90,66 @@ public class Results extends AppCompatActivity {
                             // pts 0 es el id, 1 es el puntaje sumado y 2 es la posicion del finalista
                             pts[i][0]=pk; // id del participante
                             System.out.println(pk);
+
+                            // por cada votacion
                             for(int v  =0;v<response.length();v++){
-                                Integer id_participant = Integer.parseInt( response.getJSONObject(v).getString("SK").split("#")[3]);
+                                JSONObject votacion = response.getJSONObject(v);
+                                Integer id_participant = Integer.parseInt(votacion.getString("SK").split("#")[3]);
+
+                                proyeccion = (Integer.parseInt(votacion.getString("proyeccion")));
+                                lenguaje = (Integer.parseInt(votacion.getString("lenguaje")));
+                                contenido = (Integer.parseInt(votacion.getString("contenido")));
+
+                                // si la votacion v corresponde al participante i
                                 if (id_participant == pk){
                                     System.out.println("Participant "+id_participant);
-
+                                    pts[i][1]+= (proyeccion);
+                                    pts[i][2]+= (lenguaje);
+                                    pts[i][3]+= (contenido);
+                                    pts[i][4]+= (proyeccion+lenguaje+contenido);
                                 }
                             }
+                        }
+                        System.out.println("Column 1\tColumn 2");
+
+                        Arrays.sort(pts, new Comparator<int[]>() {
+                            @Override
+                            public int compare(int[] a, int[] b) {
+                                return Integer.compare(b[1], a[1]);
+                            }
+                        });
+
+
+
+                        Integer max_index = 0;
+                        Integer posicion = 1;
+                        pts[0][5]=posicion;
+                        for(int i=1; i<pts.length;i++){
+                            int current = pts[i][1];
+                            int previous = pts[i-1][1];
+
+                            if (current==previous){
+                                max_index +=1;
+
+                            }
+                            else{
+                                max_index +=1;
+                                posicion+=1;
+                            }
+                            pts[i][5] = posicion;
+                        }
+                        for (int i = 0; i < pts.length; i++) { //part_puntaje.length
+                            for (int j = 0; j < pts[1].length; j++) {
+                                System.out.print(pts[i][j] + "\t\t");
+                            }
+                            System.out.println();
                         }
                     } catch (JSONException e) {
                         throw new RuntimeException(e);
                     }
 
 
-                try {
-                    for (int i = 0; i < response.length(); i++) {
-                        p = response.getJSONObject(i);
-                        //System.out.println("RESPONSE LENGTH() " + response.length());
-                        System.out.println("RESPONSE OBJECT " + p);
-                        id_participante = (Integer.parseInt(p.getString("SK").split("#")[3]));
-                        System.out.println("ID PARTICIPANTE " + id_participante);
-                        proyeccion = (Integer.parseInt(p.getString("proyeccion")));
-                        lenguaje = (Integer.parseInt(p.getString("lenguaje")));
-                        contenido = (Integer.parseInt(p.getString("contenido")));
-                        puntos = proyeccion + lenguaje + contenido;
-                        System.out.println("PUNTOS " + puntos);
-                        for (int j = 0; j < cantidad_parti; j++) {
-                            //for (int j = 0; j < cantidad_parti; j++) {
 
-                            //String txt = p.getString("PK");
-                            if (part_puntaje[j][0]  == id_participante) {
-                                part_puntaje[j][1]  = part_puntaje[j][1]  + puntos;
-                                System.out.println("PUNTOS AGREGADOS A PARTICIPANTE EXIXTENTE " + puntos);
-                                System.out.println("PUNTOS AGREGADOS A PARTICIPANTE EXIXTENTE (TOTAL) " + part_puntaje[j][1]);
-                                break;
-                            } else if (part_puntaje[j][0]== 0) {
-                                part_puntaje[j][0] = id_participante;
-                                part_puntaje[j][1] = puntos;
-                                System.out.println("NUEVO PARTICIPANTE EN ARREGLO " + puntos);
-                                break;
-                            }
-
-                        }
-                    }
-
-                    Arrays.sort(part_puntaje, new Comparator<int[]>() {
-                        @Override
-                        public int compare(int[] a, int[] b) {
-                            return Integer.compare(b[0], a[0]);
-                        }
-                    });
-
-                    /*for (int i = 0; i < part_puntaje.length; i++) {
-                        for (int j = 0; j < part_puntaje[i].length; j++) {
-                            ganadores[i][j] = part_puntaje[i][j];
-                        }
-                    }
-
-                    for (int i = 0; i < ganadores.length; i++) {
-                        for (int j = 0; j < ganadores[i].length; j++) {
-                            System.out.print(ganadores[i][j] + " ");
-                        }
-                        System.out.println();
-                    }*/
-
-                    System.out.println("Column 1\tColumn 2");
-                    for (int i = 0; i < cantidad_parti; i++) { //part_puntaje.length
-                        for (int j = 0; j < 2; j++) {
-                            System.out.print(part_puntaje[i][j] + "\t\t");
-                        }
-                        System.out.println();
-                    }
-
-            } catch (JSONException e) {
-                throw new RuntimeException(e);
-            }
             }
         });
 
